0691856c38cd6fec77f9e33283ca1ca3b76dd511
eugene3231 <47620989+eugene3231@users.noreply.github.com>
2020-06-17 13:44:04 +0800

    add scrolling component (sticky searchbar)

---
+++ src/Scroll.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// has children of cardList
+const Scroll = (props) => {
+    return (
+        <div style={{overflowY: 'scroll', border:'2px solid black', height:'800px'}}>
+            {props.children}
+        </div>
+    );
+};
+
+export default Scroll;
