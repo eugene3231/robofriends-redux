7bcd211ed382abc8d9b4d73999b50780a63970fc
eugene3231 <47620989+eugene3231@users.noreply.github.com>
2020-06-17 17:50:24 +0800

    add middleware logging

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react'; // does dom manipulation for us
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore , applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import { searchRobots } from './reducers';
 import 'tachyons';
 
-
-const store = createStore(searchRobots); // COMBINES REDUCERS
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger)); // COMBINES REDUCERS
 
 ReactDOM.render( 
               <Provider store={store}>
