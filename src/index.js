b59faaf0cbedc042f06a4e45a9750c1aad457350
eugene3231 <47620989+eugene3231@users.noreply.github.com>
2020-06-17 17:16:50 +0800

    add providers

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from 'react'; // does dom manipulation for us
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import { searchRobots } from './reducers';
 import 'tachyons';
 
+
+const store = createStore(searchRobots); // COMBINES REDUCERS
+
 ReactDOM.render( 
-  <App/>
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
-  ,document.getElementById('root')
+              <Provider store={store}>
+                <App />
+              </Provider>
+              // <React.StrictMode>
+              //   <App />
+              // </React.StrictMode>
+              ,document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
